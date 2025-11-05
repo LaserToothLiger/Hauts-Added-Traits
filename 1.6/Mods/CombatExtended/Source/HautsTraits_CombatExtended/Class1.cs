1aacb3f88e3b7829d711af82fbdf7408e7ccf613
LaserToothLiger <lasertoothliger@gmail.com>
2025-11-04 23:15:27 -0800

    v1.2.0
    Combat Extended compatibility. Also a few trans balance updates and niche bugfixes.

---
+++ 1.6/Mods/CombatExtended/Source/HautsTraits_CombatExtended/Class1.cs
@@ -0,0 +1,34 @@
+﻿using CombatExtended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace HautsTraits_CombatExtended
+{
+    [StaticConstructorOnStartup]
+    public static class HautsTraits_CombatExtended
+    {
+    }
+    public class Verb_Shoot_NeuralHeatAmmo_CE : Verb_ShootCE
+    {
+        public override bool Available()
+        {
+            if (this.CasterIsPawn && this.CasterPawn.psychicEntropy.EntropyValue < 1f)
+            {
+                return false;
+            }
+            return base.Available();
+        }
+        public override void WarmupComplete()
+        {
+            base.WarmupComplete();
+            if (this.CasterIsPawn)
+            {
+                this.CasterPawn.psychicEntropy.TryAddEntropy(-0.5f);
+            }
+        }
+    }
+}
